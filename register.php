af28f0821275094177c7a385c1c8923c33a14d11
Jan Roguwski <Jroguwski@gmail.com>
2023-01-21 23:06:03 +0100

    add user registration mechanism

--- register.php
+++ register.php
@@ -40,7 +40,22 @@
       $user_login = $_POST["login"];
       $user_password = $_POST["password"];
       $user_password2 = $_POST["password2"];
-      echo "$user_login, $user_password, $user_password2<br>";
+      // echo "$user_login, $user_password, $user_password2<br>";
+      if ($user_password != $user_password2) {
+        echo "Hasła nie są takie same";
+      }
+      else {
+        $query = "INSERT INTO USERS (LOGIN, PASSWORD) VALUES ('$user_login', '$user_password')";
+        $statement = oci_parse($connection, $query);
+        $r = oci_execute($statement);
+        if (!$r) {
+          $e = oci_error($statement);
+          echo $e['message'];
+        }
+        else {
+          echo "Zarejestrowano użytkownika $user_login";
+        }
+      }
     }
     ?>
     </body>
